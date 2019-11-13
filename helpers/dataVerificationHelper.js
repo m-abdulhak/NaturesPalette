93844fed9e7c08de2811dcd910290e4cf1b964bc
MeghaNagari <nagari_megha1@yahoo.co.in>
2019-11-13 16:17:16 -0330

    added validations

--- helpers/dataVerificationHelper.js
+++ helpers/dataVerificationHelper.js
@@ -4,6 +4,20 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 
+meta_file_obj_arr = ["InstitutionCode",
+"CollectionCode",
+"CatalogNumber",
+"Genus",
+"SpecificEpithet",
+"InfraspecificEpithet",
+"Sex",
+"Country",
+"Part",
+"Replicate",
+"File name"
+]
+
+
 exports.verifyUploadRequest = function(req,err) {
   // verify request has exactly 2 files
   if (!req.files || Object.keys(req.files).length != 2) {
@@ -20,11 +34,23 @@ exports.verifyUploadRequest = function(req,err) {
   	err.details = "Metadata file is missing or selected file type is not supported!"
     return false;
   }
-
-  // TODO: verify request includes expected parameters (fname,lname,metaFile,rawFile,....)
-  if (!req.body.fname) {
-  	error.details = "Not all required parameters provided!"
-    return false;
+  
+  // verify if each column is existing
+  var array = [];
+  var fileContents = fs.readFileSync('./uploads/'+req.files.metaFile.name);
+  var lines = fileContents.toString().split('\n');
+  string = lines[0].toString();
+  array = string.split(',');
+  missingColumn = []
+for(var i =0;i<meta_file_obj_arr.length;i++)
+// check = meta_file_obj_arr[i].replace(/\s/g, "");
+  if(!array.includes(meta_file_obj_arr[i])){
+    missingColumn.push(meta_file_obj_arr[i]);
+  }
+  
+  if(missingColumn.length > 0){
+  err.details = "Sorry, the following fields are missing: "+missingColumn.join();
+  return false;
   }
 
   // TODO: All Other 'online' request-related verifications 
