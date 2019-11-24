152a6a6b58bcbe10021c33a4ff8c601a051c1a4a
MeghaNagari <nagari_megha1@yahoo.co.in>
2019-11-24 15:58:28 -0330

    added download zip code

--- helpers/zipHelper.js
+++ helpers/zipHelper.js
@@ -6,7 +6,10 @@ const app = express();
 
 const uuidv1 = require('uuid/v1');
  
-var AdmZip = require('adm-zip');
+// var AdmZip = require('adm-zip');
+
+var zip = new require('node-zip')();
+
 
 const rawFilesLocation = path.dirname(__dirname) + '/uploads/raw/';
 
@@ -44,17 +47,34 @@ get_files = function(folder) {
 
 
 exports.zip = function(fileUrls){
-    // creating archives
-    var zip = new AdmZip();
-    var rand = uuidv1();
 
-    for(var file of fileUrls){
-      // add local file
-      zip.addLocalFile(file);      
+  for(var file of fileUrls){
+    fn_split  = file.split("\\")
+    fn = fn_split[fn_split.length-1]
+    if(fn.includes(".csv")){
+      spli_fn = fn.split("/")
+      meta_file = spli_fn[spli_fn.length-1]
+      zip.file(meta_file,fs.readFileSync(file));
+      break;
     }
-    //
-    var willSendthis = zip.toBuffer();
-    return willSendthis;
+     zip.file(fn,fs.readFileSync(file));
+   }
+   var data = zip.generate({base64:true,compression:'DEFLATE'});
+   // console.log(data); // ugly data
+   return data;
+
+
+    // creating archives
+    // var zip = new AdmZip();
+    // var rand = uuidv1();
+
+    // for(var file of fileUrls){
+    //   // add local file
+    //   zip.addLocalFile(file);      
+    // }
+    // //
+    // var willSendthis = zip.toBuffer();
+    // return willSendthis;
     // or write everything to disk
     //var zipFileUrl = 'downloads/researchData'+ rand + '.zip';
     //zip.writeZip(zipFileUrl);
